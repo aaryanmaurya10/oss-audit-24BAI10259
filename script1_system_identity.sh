f7bf436629e3451e53bcc5c5f2e1863b29595d36
Warpy <65025065+aaryanmaurya10@users.noreply.github.com>
2026-03-25 22:09:04 +0530

    Update script1_system_identity.sh

--- script1_system_identity.sh
+++ script1_system_identity.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # ============================================================
 # Script 1: System Identity Report
-# Author: [Your Name] | Roll: [Your Roll Number]
+# Author: Aaryan Maurya | Roll: 24BAI10259
 # Course: Open Source Software | Software Choice: Git
 # Description: Displays a welcome screen with system info
 # ============================================================
 
 # --- Variables ---
-STUDENT_NAME="[Your Name]"        # Replace with your name
-SOFTWARE_CHOICE="Git"             # Our chosen open-source software
-LICENSE="GPL v2"                  # Git's license
+STUDENT_NAME="Aaryan Maurya"
+SOFTWARE_CHOICE="Git"
+LICENSE="GPL v2"
 
 # --- Gather system information using command substitution ---
-KERNEL=$(uname -r)                          # Get kernel version
-USER_NAME=$(whoami)                         # Get current logged-in user
-HOME_DIR=$HOME                              # Get home directory path
-UPTIME=$(uptime -p)                         # Get human-readable uptime
-CURRENT_DATE=$(date '+%d %B %Y %H:%M:%S')  # Get current date and time
-DISTRO=$(lsb_release -d | cut -f2)         # Get Linux distro name
+KERNEL=$(uname -r)                          
+USER_NAME=$(whoami)                         
+HOME_DIR=$HOME                              
+UPTIME=$(uptime -p)                         
+CURRENT_DATE=$(date '+%d %B %Y %H:%M:%S')  
+DISTRO=$(lsb_release -d | cut -f2)        
 
 # --- Display formatted output ---
 echo "=================================================="
