25ad38a8d41fb74954d7a1a4db84a4161df971ae
Warpy <65025065+aaryanmaurya10@users.noreply.github.com>
2026-03-25 22:11:40 +0530

    Update script4_log_analyzer.sh

--- script4_log_analyzer.sh
+++ script4_log_analyzer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 4: Log File Analyzer
-# Author: [Your Name] | Roll: [Your Roll Number]
+# Author: Aaryan Maurya | Roll: 24BAI10259
 # Course: Open Source Software | Software Choice: Git
 # Description: Reads a log file, counts keyword occurrences,
 #              and shows the last 5 matching lines
